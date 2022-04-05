4054e42f31fa9308a094b127d71338f8dcd147cd
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-04-05 03:45:36 -0700

    Added : Exception Handling

--- src/test/java/com/blz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/blz/moodanalyser/MoodAnalyserTest.java
@@ -4,41 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
+
     @Test
     public void givenSadWhileAnalysisMoodShouldReturnSad() {
-        MoodAnalyser moodAnalyzer = new MoodAnalyser("I am in Sad Mood");
-        try {
-            Assertions.assertEquals("SAD",moodAnalyzer.analyseMood());
-        } catch (MoodAnalyzerException e) {
-        }
+        MoodAnalyser MoodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        String result = MoodAnalyser.analyseMood();
+        Assertions.assertEquals("SAD", result);
     }
 
     @Test
     public void givenHappyWhileAnalysisMoodShouldReturnHappy() {
-        MoodAnalyser moodAnalyzer = new MoodAnalyser("I am in Happy Mood");
-        try {
-            Assertions.assertEquals("HAPPY",moodAnalyzer.analyseMood());
-        } catch (MoodAnalyzerException e) {
-        }
+        MoodAnalyser MoodAnalyser = new MoodAnalyser("I am in Happy Mood");
+        String result = MoodAnalyser.analyseMood();
+        Assertions.assertEquals("HAPPY", result);
     }
 
     @Test
     public void givenNullWhileAnalysisMoodShouldReturnHappy() {
-        MoodAnalyser moodAnalyzer = new MoodAnalyser(null);
-        try {
-            moodAnalyzer.analyseMood();
-        } catch (MoodAnalyzerException e) {
-            Assertions.assertEquals(MoodAnalyzerException.ExceptionType.ENTERED_NULL, e.getType());
-        }
-    }
-
-    @Test
-    public void givenEmptyWhileAnalysisMoodShouldReturnHappy() {
-        MoodAnalyser moodAnalyzer = new MoodAnalyser("");
-        try {
-            moodAnalyzer.analyseMood();
-        } catch (MoodAnalyzerException e) {
-            Assertions.assertEquals(MoodAnalyzerException.ExceptionType.ENTERED_EMPTY, e.getType());
-        }
+        MoodAnalyser MoodAnalyser = new MoodAnalyser(null);
+        String result = MoodAnalyser.analyseMood();
+        Assertions.assertEquals("HAPPY", result);
     }
 }
